de02329ba5fa76ac2c133c2de75ef6c36c5fd51d
Carlos Kiran Subramanian Vidal <cksubram@usc.edu>
2022-11-14 21:33:52 -0800

    Sever now is a thread

--- ServerClass.java
+++ ServerClass.java
@@ -8,89 +8,84 @@ import java.util.Collections;
 import java.util.List;
 
 
-public class ServerClass {
+public class ServerClass extends Thread {
 	
-	public static void main(String[] args) {
-		
+	private int connections;
+	private int port;
+	private ServerSocket server;
+	private Socket socket;
+	private boolean inGame;
+	
+	//Synchronised list of client threads
+	List<ServerThread> players = Collections.synchronizedList(new ArrayList<ServerThread>());
+	List<ServerThread> waitingPlayers = Collections.synchronizedList(new ArrayList<ServerThread>());
+	
+	
+	public ServerClass() {
 		//game thread and connecting thread
 		
 		//value for number of connected clients
-		int connections = 0;
-		
+				
+		connections = 0;
+				
 		//port, intialise the server
-		int port = 3456;
-		ServerSocket server = null;
-		Socket socket;
-		
-		
-		//Synchronised list of client threads
-		List<ServerThread> players = Collections.synchronizedList(new ArrayList<ServerThread>());
-		
-		
+		port = 3456;
+		server = null;
+		inGame = false;
+				
+			
+				
+				
 		try {
 			server = new ServerSocket(port);
-			
+					
 		}catch (Exception e) {
 		}
-		
-		
+				
+				
 		//create a deck for the game
-			
-		
-		
+					
+				
+				
 		while(true) {
-			
+					
 			if(connections < 1) {
-				System.out.println("Waiting for at least one connection");
-				
-				try {
-					socket = server.accept();
-					
-					//create client thread, add it to the LIST
-					connections++;
-					
-					ServerThread tempServerThread = new ServerThread(socket, port);
-					tempServerThread.sendMessage("Welcome!");
-					tempServerThread.sendMessage("You are player " + connections);
-					
-					
-					players.add(tempServerThread);		
-				} catch (Exception e) {
-				}
-				
-				
+					System.out.println("Waiting for at least one connection");
+						
+					try {
+						socket = server.accept();
+							
+						//create client thread, add it to the LIST
+						connections++;
+							
+						ServerThread tempServerThread = new ServerThread(socket, port);
+						tempServerThread.sendMessage("Welcome!");
+						tempServerThread.sendMessage("You are player " + connections);
+							
+							
+						players.add(tempServerThread);		
+						
+						this.start();
+						
+					} catch (Exception e) {
+					}
+						
+						
 			} else {
-				
+						
 				System.out.println("There are currently " + connections + " Player(s)");
-				
-				
-				
+						
+						
+						
 				//Play game + look for new connections
 				
-				
-				//look for new connections
-				
-				//make a new thread for this? Will be blocked by next loop
-				try {
-					socket = server.accept();
-					
-					//create client thread, add it to the list
-					connections++;
-					
-					ServerThread tempServerThread = new ServerThread(socket, port);
-					tempServerThread.sendMessage("Welcome!");
-					tempServerThread.sendMessage("You are player " + connections);
-					
-					
-					players.add(tempServerThread);	
-					
-				} catch (Exception e) {
+				while(!waitingPlayers.isEmpty()) {
+					Thread.yield();
 				}
-				
-				
-							
-				
-				
+						
+									
+						
+						
 				//prompt for start
 				for(int i = 0; i < players.size(); i++) {
 					players.get(i).sendMessage("There are currently " + connections + " Player(s)");
@@ -98,50 +93,52 @@ public class ServerClass {
 					players.get(i).inputWaiter();
 					players.get(i).startBetting();
 				}
-				
-				
+						
+						
 				int betsMade = 0;
 				while(betsMade < players.size()) {
 					betsMade = 0;
 					for(int i = 0; i < players.size(); i++) {
-						if(players.get(i).getBetting()) {
+						if(!players.get(i).getBetting()) {
 							betsMade++;
 						}
 					}
 				}
 				
-				
-				
-				
-				
+				inGame = true;
+						
+						
+						
+						
+						
 				//play the game,
 				//Potentially acquire a lock here to prevent new connections
-				
-				
+						
+						
 				//get the cards
-				
+						
 				//first the dealer
 				int dealerSum = 0;
 				//let the dealer take two cards
-				
-				
-				
+						
+						
+						
 				//deal two cards to each player
 				for(int i = 0; i < players.size(); i++) {
 					players.get(i).sendMessage(players.get(i).cardString());
 				}
-				
-				
+						
+						
 				//loop through all the players until they are done playing
 				int playersDone = 0;
 				while(playersDone < players.size()) {
 					playersDone = 0;
-					
+							
 					for(int i = 0; i < players.size(); i++) {
 						//if the player is not playing add to sum
 						if(!players.get(i).getPlaying()) {
 							playersDone++;
-							
+									
 						} else {
 							//if playing and wants card
 							if(players.get(i).wantsCard()) {
@@ -149,8 +146,10 @@ public class ServerClass {
 							}
 						}
 					}
-				}	
-			
+				}
+				
+				inGame = false;
+					
 				//check player win/loss and reset the player threads
 				for(int i = 0; i < players.size(); i++) {
 					if(players.get(i).getSum() >= dealerSum && players.get(i).getSum() <= 21) {
@@ -160,14 +159,52 @@ public class ServerClass {
 						players.get(i).sendMessage("You lost " + players.get(i).getBet());
 						players.get(i).loss();
 					}
-					
+							
 					players.get(i).sendMessage("Your balance is " + players.get(i).getBalance());
 				}
-				
-		
-				
-				
+						
+								
 			}			
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		ServerClass serverClass = new ServerClass();
+		
+	}
+	
+	
+	public void run() {
+		//look for new connections
+		
+		while(connections < 4) {
+			//accept new connections
+			
+			if(!inGame) {
+				players.addAll(waitingPlayers);
+				waitingPlayers.clear();
+			}
+			
+			
+			try {
+				socket = server.accept();
+			
+				//create client thread, add it to the list
+				connections++;
+			
+				ServerThread tempServerThread = new ServerThread(socket, port);
+				tempServerThread.sendMessage("Welcome!");
+				tempServerThread.sendMessage("You are player " + connections);
+			
+				if(!inGame) {
+					players.add(tempServerThread);	
+				} else {
+					waitingPlayers.add(tempServerThread);
+				}
+			
+			} catch (Exception e) {
+			}
 		}
 	}
 }
